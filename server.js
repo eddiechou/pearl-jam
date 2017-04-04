2b24d1e2fdefb70e7c36bb8fa64129c8a0eb4163
jdbriner07 <jdbriner07@gmail.com>
2017-04-04 16:01:50 -0700

    demoing heroku pipelines

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('hello world');
+	res.send('hi mike');
 })
 
 app.listen(process.env.PORT || 3000, function() {
