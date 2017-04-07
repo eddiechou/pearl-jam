c94538319defe0bbe712d6ba7ede0997edc723e7
Eddie Chou <eddieechou@gmail.com>
2017-04-07 01:20:06 -0700

    (refactor) Fix typo

--- mainServer/mainServer.js
+++ mainServer/mainServer.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, function() {
