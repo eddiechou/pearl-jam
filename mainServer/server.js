bafb22f2f5e5ff902b2efcc33728e24d5d7e4f3c
Eddie Chou <eddieechou@gmail.com>
2017-04-06 21:20:16 -0700

    (feat) Initial main web server configuration.
    Will serve client/dist/index.html.

--- mainServer/server.js
+++ mainServer/server.js
@@ -1,11 +1,14 @@
-var express = require("express");
+var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send('hi mike');
-})
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(process.env.PORT || 3000, function() {
-	console.log('server is listening');
-})
+  console.log(`mainServer is listening on PORT ${process.env.port || 3000}`);
+});
