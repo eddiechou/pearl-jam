0cd161a4db5effa42e2b2d53cfc865d936af52ec
Eddie Chou <eddieechou@gmail.com>
2017-04-06 23:10:57 -0700

    (refactor) Use ES5 function instead of arrow function

--- mainServer/mainServer.js
+++ mainServer/mainServer.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('*', (req, res) => {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
