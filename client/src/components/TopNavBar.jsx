15df0382417a14834f7923c20fa0401df0145d0c
Eddie Chou <eddieechou@gmail.com>
2017-04-07 01:05:19 -0700

    (feat) Add ReactRouter Links to Login, PlayerView and SpectatorView components

--- client/src/components/TopNavBar.jsx
+++ client/src/components/TopNavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Link } from 'react-router-dom';
 
 export default class TopNavBar extends React.Component {
 
@@ -9,7 +10,11 @@ export default class TopNavBar extends React.Component {
 
   render() {
     return (
-      <h1>TopNavBar</h1>
+      <ul>
+        <li><a href="/login">Login Page</a></li>
+        <li><Link to="/playerView">Player View</Link></li>
+        <li><Link to="/spectatorView">Spectator View</Link></li>
+      </ul> 
     );
   }
 }
