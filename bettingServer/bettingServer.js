c56dadbd19a6d755e4b02d87075cb84e45b9386f
Eddie Chou <eddieechou@gmail.com>
2017-04-07 17:40:07 -0700

    (feat) Implement eloCalc library and test in bettingServer

--- bettingServer/bettingServer.js
+++ bettingServer/bettingServer.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+var eloCalc = require('./elo');
+
+// TODO (Eddie): Connect to database
 app.get('/', function(req, res) {
   res.send('Hello from the bettingServer');
 });
 
+
+// Create route to submit a bet to the betting server
+// A bet needs:   gameID, projectedWinner, amount, bettorID
+app.post('/bet', function(req, res) {
+  // Make a bet
+  // TODO (Eddie): Submit a bet to the database
+
+  res.sendStatus(201);
+});
+
+// Get the odds for a particular gameID
+app.get('/odds/:playerA/:playerB', function(req, res) {
+  var gameID = req.params.gameId;
+  
+  // TODO (Eddie): request database for the odds of a gameID
+
+  var eloPlayerA = 1600;
+  var eloPlayerB = 1670;
+
+  var expectedValue = eloCalc.getOddsOfWin(eloPlayerA, eloPlayerB);
+
+  res.json(expectedValue);
+
+});
 
 app.listen(process.env.PORT || 3500, function() {
   console.log(`bettingServer is listening on PORT ${process.env.port || 3500}`);
