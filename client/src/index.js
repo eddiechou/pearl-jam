c27a2c009adde8d9b6b7090fc460d79a321b0ab8
Mycah Jay Anast <mycahjay@gmail.com>
2017-04-10 13:40:55 -0700

    amended package json

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,12 @@ import { Provider } from 'react-redux'
 
 import reduxStore from './reduxStore'
 
-/* * RedBox just makes errors fun * */
-import RedBox from 'redbox-react'
 import App from './App'
 
 const root = document.getElementById('root')
 
 /* * wrapping App.js in Proivder component to allow access to our redux store * */
-try {
-  render(
-    <Provider store={reduxStore}>
-      <App />
-    </Provider>, root)
-} catch (e) {
-  render(<RedBox error={e} />, root)
-}
+render(
+  <Provider store={reduxStore}>
+    <App />
+  </Provider>, root)
