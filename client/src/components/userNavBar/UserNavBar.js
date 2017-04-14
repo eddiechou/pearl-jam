838b8eb0e1424cff5811482d4e9ad07eff18b07e
Mycah Jay Anast <mycahjay@gmail.com>
2017-04-14 16:52:04 -0700

    basic user nav bar implemented

--- client/src/components/userNavBar/UserNavBar.js
+++ client/src/components/userNavBar/UserNavBar.js
@@ -2,26 +2,23 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import BaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
-import * as mui from 'material-ui'
-
 import FlatButton from 'material-ui/FlatButton'
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
 
 import NavDropMenu from '../navDropMenu/NavDropMenu'
 
 class UserNavBar extends Component {
+  checkProps () {
+    console.log(this.props)
+  }
   render () {
-    const { uploadBill, logOut } = this.props
-
     return (
       <Toolbar>
         <ToolbarGroup>
           <ToolbarTitle text='Pearl Jam' />
         </ToolbarGroup>
         <ToolbarGroup>
+          <FlatButton label='Props' onClick={this.checkProps.bind(this)} />
           <FlatButton label='Watch' />
           <FlatButton label='Play' secondary />
           <NavDropMenu />
