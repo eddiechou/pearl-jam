ca2f1c86f70991db1fbc1c271a8f65d71e753c99
Eddie Chou <eddieechou@gmail.com>
2017-04-22 10:33:30 -0700

    (feat) Create stubs for firebase calls in user.test.js

--- __tests__/reducers/user.test.js
+++ __tests__/reducers/user.test.js
@@ -1,11 +1,16 @@
 import user from '../../client/src/reducers/user'
+import sinon from 'sinon'
+
+import { firebaseApp } from '../../client/src/base'
+
+const base = firebaseApp.database()
 
 describe('User Reducer', () => {
   it('has a default state', () => {
     expect(user(undefined, {type: 'expected'})).toEqual({})
   })
 
-  xit('can handle SET_USER', () => {
+  it('can handle SET_USER action', () => {
     const type = 'SET_USER'
     const payload = {
       uid: '111',
@@ -19,7 +24,12 @@ describe('User Reducer', () => {
     expect(user(undefined, {type, payload})).toHaveProperty('photoURL')
   })
 
-  xit('can handle CREATE_NEW_USER', () => {
+  it('can handle CREATE_NEW_USER action', () => {
+    // Makes base.ref().set into a stub
+    var stub = sinon.stub(base, 'ref', () => {
+      return {set: () => {}}
+    });
+
     const type = 'CREATE_NEW_USER'
     const payload = {
       uid: '111',
@@ -28,19 +38,19 @@ describe('User Reducer', () => {
     expect(user(undefined, {type, payload})).toHaveProperty('uid')
     expect(user(undefined, {type, payload})).toHaveProperty('email')
     expect(user(undefined, {type, payload})).toHaveProperty('photoURL')
+
+    stub.restore()
   })
 
-  xit('can handle SET_DISPLAY_NAME', () => {
+  xit('can handle UPDATE_USER_INFO action', () => {
     const type = 'SET_DISPLAY_NAME'
     const payload = {displayName: 'test'}
     expect(user(undefined, {type, payload})).toHaveProperty('displayName')
   })
 
-  it('can handle SET_GAME', () => {
+  it('can handle SET_GAME action', () => {
     const type = 'SET_GAME'
     const payload = {gameID: 5}
     expect(user(undefined, {type, payload})).toHaveProperty('gameID', 5)
   })
-
-
 })
