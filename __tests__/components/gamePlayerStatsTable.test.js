7fcc33d7b28c425da02c3ce7f7536251752afad5
Eddie Chou <eddieechou@gmail.com>
2017-04-22 10:34:48 -0700

    (feat) Add Enzyme tests for gamePlayerStatsTable

---
+++ __tests__/components/gamePlayerStatsTable.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import GamePlayerStatsTable from '../../client/src/components/gamePlayerStatsTable/gamePlayerStatsTable'
+
+describe('<GamePlayerStatsTable/>', () => {
+  const game = {
+    players: [
+      {
+        displayName: 'eddie',
+        rating: 1300
+      },
+      {
+        displayName: 'jeff',
+        rating: 1400
+      }
+    ]
+  }
+  it('should render 3 TableHeaderColumns', () => {
+    const wrapper = shallow(<GamePlayerStatsTable game={game}/>)
+    expect(wrapper.find('TableHeaderColumn').length).toBe(3)
+  })
+
+  it('should have player, rating, and odds columns', () => {
+    const wrapper = shallow(<GamePlayerStatsTable game={game}/>)
+    // TODO: Check if the texts exist
+    expect(wrapper.find('Player').length).toBe(1)
+  })
+})
