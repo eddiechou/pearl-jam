c47fe372bd21c7580ecb99a533e7452c0ae8bf12
Mycah Jay Anast <mycahjay@gmail.com>
2017-04-24 21:41:50 -0700

    fixed new async problems with firebase

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -16,24 +16,31 @@ const user = (state = {}, action) => {
       const { uid, email, photoURL } = action.payload
       const newState = { uid, email, photoURL }
       base.ref(`users/${uid}`)
-      .set({ displayName: null, email: email, photoURL: photoURL, avatar: null, pearls: 100, rating: 1200, wins: 0, losses: 0 })
+      .set({
+        displayName: null,
+        email: email,
+        photoURL: photoURL,
+        avatar: null,
+        pearls: 100,
+        rating: 1200,
+        wins: 0,
+        losses: 0
+      })
       return newState
     }
 
     case UPDATE_USER_INFO: {
       console.log('updating user info')
       const { uid, displayName, avatarColorID } = action.payload
-      console.log('info', uid, displayName, avatarColorID)
       const baseUser = auth.currentUser
       const newState = Object.assign({}, state)
+      newState.displayName = displayName
+      newState.avatar = avatarColorID
+
       baseUser.updateProfile({ displayName })
       .then(() => {
         base.ref(`users/${uid}`)
         .update({ displayName, avatar: avatarColorID })
-      })
-      .then(() => {
-        newState.displayName = displayName
-        newState.avatar = avatarColorID
       })
       return newState
     }
