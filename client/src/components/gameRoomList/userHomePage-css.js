e02df3d8bd69fc1a1d2b4d23a724b31c4ac82848
Mike Liao <mikeliao97@gmail.com>
2017-04-25 18:52:58 -0700

    add game list styles

---
+++ client/src/components/gameRoomList/userHomePage-css.js
@@ -0,0 +1,80 @@
+const style = {
+  container: {
+    margin: '0',
+    overflow: 'hidden',
+    // backgroundColor: '#000000',
+    // backgroundColor: '#ffbfda',
+    display: 'flex',
+    alignItems: 'center',
+    height: '100vh',
+    justifyContent: 'center'
+  },
+  createButton: {
+    marginRight: '50px',
+    border: '4px solid #ffffff',
+    backgroundColor: 'transparent',
+    color: '#ffffff',
+    fontSize: '20px',
+    padding: '30px 60px',
+    margin: '10px',
+    fontFamily: 'PT Mono, serif',
+    cursor: 'pointer'
+  },
+  createButtonHover: {
+    marginRight: '50px',
+    border: '4px solid #ffffff',
+    backgroundColor: '#ffffff',
+    color: '#000000',
+    fontSize: '20px',
+    padding: '30px 60px',
+    margin: '10px',
+    fontFamily: 'PT Mono, serif',
+    cursor: 'pointer'
+  },
+  button1: {
+    marginLeft: '50px',
+    border: '4px solid #ffffff',
+    backgroundColor: 'transparent',
+    color: '#ffffff',
+    fontSize: '20px',
+    padding: '30px 60px',
+    margin: '10px',
+    fontFamily: 'PT Mono, serif',
+    cursor: 'pointer'
+  },
+  buttonHover1: {
+    marginLeft: '50px',
+    border: '4px solid #ffffff',
+    backgroundColor: '#ffffff',
+    color: '#000000',
+    fontSize: '20px',
+    padding: '30px 60px',
+    margin: '10px',
+    fontFamily: 'PT Mono, serif',
+    cursor: 'pointer'
+  },
+  button2: {
+    marginLeft: '50px',
+    border: '4px solid #ffffff',
+    backgroundColor: 'transparent',
+    color: '#ffffff',
+    fontSize: '20px',
+    padding: '30px 60px',
+    margin: '10px',
+    fontFamily: 'PT Mono, serif',
+    cursor: 'pointer'
+  },
+  buttonHover2: {
+    marginLeft: '50px',
+    border: '4px solid #ffffff',
+    backgroundColor: '#ffffff',
+    color: '#000000',
+    fontSize: '20px',
+    padding: '30px 60px',
+    margin: '10px',
+    fontFamily: 'PT Mono, serif',
+    cursor: 'pointer'
+  }
+}
+
+export default style
